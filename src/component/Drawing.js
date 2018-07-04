8a4312da049c79f0a1e2e6869b6979988c5d1df7
Tonpai <itonpai.bkk@gmail.com>
2018-07-04 13:49:02 +0700

    Separate Pencil.js

--- src/component/Drawing.js
+++ src/component/Drawing.js
@@ -1,67 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { toolConstant } from "../redux/constant";
 
 import { Image } from "react-konva";
-
-class Pencil {  
-  constructor(state){
-    this._state = state;
-  }
-
-  componentDidMount(){
-    const canvas = document.createElement("canvas");
-    canvas.width = 300;
-    canvas.height = 300;
-    const context = canvas.getContext("2d");
-
-    this._state.setState({ canvas, context });
-  }
-  handleMouseDown(){
-    this._state.setState({ isDrawing: true });
-    const stage = this._state.image.getStage();
-    this._state.lastPointerPosition = stage.getPointerPosition();
-  }
-  handleMouseUp(){
-    this._state.setState({ isDrawing: false });
-  }
-  handleMouseMove(){
-    const { context, isDrawing } = this._state.state;
-
-    if (isDrawing) {
-      context.strokeStyle = "#df4b26";
-      context.lineJoin = "round";
-      context.lineWidth = 5;
-
-      if(this._state.props.tool === toolConstant.PENCIL){
-        context.globalCompositeOperation = "source-over";
-      } else if(this._state.props.tool === toolConstant.ERASER){
-        context.globalCompositeOperation = "destination-out";
-      }
-
-      context.beginPath();
-
-      var localPos = {
-        x: this._state.lastPointerPosition.x - this._state.image.x(),
-        y: this._state.lastPointerPosition.y - this._state.image.y()
-      };
-      context.moveTo(localPos.x, localPos.y);
-
-      const stage = this._state.image.getStage();
-
-      var pos = stage.getPointerPosition();
-      localPos = {
-        x: pos.x - this._state.image.x(),
-        y: pos.y - this._state.image.y()
-      };
-      context.lineTo(localPos.x, localPos.y);
-      context.closePath();
-      context.stroke();
-      this._state.lastPointerPosition = pos;
-      this._state.image.getLayer().draw();
-    }
-  }
-}
+import Pencil from './Pencil';
 
 class Drawing extends Component {
 
@@ -93,7 +34,6 @@ class Drawing extends Component {
 
   render() {
     const { canvas } = this.state;
-
     return (
       <Image
         image={canvas}
---
+++ src/component/Pencil.js
@@ -0,0 +1,62 @@
+import { toolConstant } from "../redux/constant";
+
+class Pencil {  
+  constructor(state){
+    this._state = state;
+  }
+
+  componentDidMount(){
+    const canvas = document.createElement("canvas");
+    canvas.width = 300;
+    canvas.height = 300;
+    const context = canvas.getContext("2d");
+
+    this._state.setState({ canvas, context });
+  }
+  handleMouseDown(){
+    this._state.setState({ isDrawing: true });
+    const stage = this._state.image.getStage();
+    this._state.lastPointerPosition = stage.getPointerPosition();
+  }
+  handleMouseUp(){
+    this._state.setState({ isDrawing: false });
+  }
+  handleMouseMove(){
+    const { context, isDrawing } = this._state.state;
+
+    if (isDrawing) {
+      context.strokeStyle = "#df4b26";
+      context.lineJoin = "round";
+      context.lineWidth = 5;
+
+      if(this._state.props.tool === toolConstant.PENCIL){
+        context.globalCompositeOperation = "source-over";
+      } else if(this._state.props.tool === toolConstant.ERASER){
+        context.globalCompositeOperation = "destination-out";
+      }
+
+      context.beginPath();
+
+      var localPos = {
+        x: this._state.lastPointerPosition.x - this._state.image.x(),
+        y: this._state.lastPointerPosition.y - this._state.image.y()
+      };
+      context.moveTo(localPos.x, localPos.y);
+
+      const stage = this._state.image.getStage();
+
+      var pos = stage.getPointerPosition();
+      localPos = {
+        x: pos.x - this._state.image.x(),
+        y: pos.y - this._state.image.y()
+      };
+      context.lineTo(localPos.x, localPos.y);
+      context.closePath();
+      context.stroke();
+      this._state.lastPointerPosition = pos;
+      this._state.image.getLayer().draw();
+    }
+  }
+}
+
+export default Pencil;
